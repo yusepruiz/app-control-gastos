5b46cef42fed9fd3cf6797a6b709099f1db81dd5
Yusep Ruiz <yusep3012@hotmail.com>
2026-04-27 00:42:37 -0500

    Se añaden los nuevos widgets del login

--- lib/features/auth/presentation/widgets/widgets.dart
+++ lib/features/auth/presentation/widgets/widgets.dart
@@ -1,2 +1,4 @@
-export 'package:gestion_gastos/features/auth/presentation/widgets/login/login_form.dart';
 export 'package:gestion_gastos/features/auth/presentation/widgets/login/auth_header.dart';
+export 'package:gestion_gastos/features/auth/presentation/widgets/login/login_actions.dart';
+export 'package:gestion_gastos/features/auth/presentation/widgets/login/login_form.dart';
+export 'package:gestion_gastos/features/auth/presentation/widgets/login/signup_link.dart';
