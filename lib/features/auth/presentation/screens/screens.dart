2a237132416ac5466dedcb6870badff18f065244
Yusep Ruiz <yusep3012@hotmail.com>
2026-05-13 05:51:37 -0500

    Se crea la pantalla inicial para el dashboard

--- lib/features/auth/presentation/screens/screens.dart
+++ lib/features/auth/presentation/screens/screens.dart
@@ -1,2 +1,3 @@
-export 'package:gestion_gastos/features/auth/presentation/screens/signup/signup_screen.dart';
+export 'package:gestion_gastos/features/auth/presentation/screens/dashboard/dashboard_screen.dart';
 export 'package:gestion_gastos/features/auth/presentation/screens/login/login_screen.dart';
+export 'package:gestion_gastos/features/auth/presentation/screens/signup/signup_screen.dart';
