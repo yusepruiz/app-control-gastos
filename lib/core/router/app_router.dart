0e34497e09276741c11d730a8c04d253f2f38e19
Yusep Ruiz <yusep3012@hotmail.com>
2026-05-13 05:51:51 -0500

    Se crea la ruta para navegar al dashboard

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -9,5 +9,9 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(path: '/signup', builder: (context, state) => const SignupScreen()),
+    GoRoute(
+      path: '/dashboard',
+      builder: (context, state) => const DashboardScreen(),
+    ),
   ],
 );
